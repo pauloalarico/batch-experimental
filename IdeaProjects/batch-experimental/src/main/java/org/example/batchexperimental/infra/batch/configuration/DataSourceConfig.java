8f7ffed9e25aee3848aecf6ca165b3aabc8199ab
pauloalarico <paulocraft20@gmail.com>
2026-02-05 23:52:48 -0300

    refactor - add the datasource

---
+++ IdeaProjects/batch-experimental/src/main/java/org/example/batchexperimental/infra/batch/configuration/DataSourceConfig.java
@@ -0,0 +1,32 @@
+package org.example.batchexperimental.infra.batch.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.datasource")
+@Getter
+@Setter
+public class DataSourceConfig {
+    private String username;
+    private String password;
+    private String url;
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        return new HikariDataSource(config);
+    }
+}
