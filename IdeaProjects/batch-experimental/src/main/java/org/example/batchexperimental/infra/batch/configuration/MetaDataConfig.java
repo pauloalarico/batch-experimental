d3a927a0c4ecc6899eaa7b1ed94c9a60f4d5e9dd
pauloalarico <paulocraft20@gmail.com>
2026-02-05 23:48:14 -0300

    refactor - add the config for the metadata of the jobs

---
+++ IdeaProjects/batch-experimental/src/main/java/org/example/batchexperimental/infra/batch/configuration/MetaDataConfig.java
@@ -0,0 +1,34 @@
+package org.example.batchexperimental.infra.batch.configuration;
+
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JdbcJobRepositoryFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class MetaDataConfig {
+    @Bean
+    @Primary
+    public JobRepository jobRepositoryMeta (DataSource dataSource,
+                                            PlatformTransactionManager transactionManager) throws Exception {
+        JdbcJobRepositoryFactoryBean factory = new JdbcJobRepositoryFactoryBean();
+        factory.setTransactionManager(transactionManager);
+        factory.setDataSource(dataSource);
+        factory.setDatabaseType("POSTGRES");
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
